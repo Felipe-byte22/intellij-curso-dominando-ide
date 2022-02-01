911e7604c3de501a9e167e260fef202aea3b000c
Felipe-byte22 <felipescunha21@gmail.com>
2022-02-01 20:40:19 -0300

    First Commit

---
+++ src/br/com/dio/primeiro/programa.java
@@ -0,0 +1,15 @@
+package br.com.dio.primeiro;
+
+import br.com.dio.primeiro.model.Gato;
+
+public class programa {
+    public static void main(String[] args){
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World " + (a+b));*/
+    }
+}
