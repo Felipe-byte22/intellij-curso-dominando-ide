806b97ae368f9a8301f29654fea7500b4db3624e
Felipe-byte22 <felipescunha21@gmail.com>
2022-02-01 22:27:22 -0300

    Criando class Calculadora 2

--- src/br/com/dio/primeiro/programa.java
+++ src/br/com/dio/primeiro/programa.java
@@ -3,7 +3,7 @@ package br.com.dio.primeiro;
 import br.com.dio.primeiro.model.Gato;
 
 public class programa {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Gato gato = new Gato();
 
         System.out.println(gato);
